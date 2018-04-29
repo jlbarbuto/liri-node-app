be6fbab11e28d419df910df4f7e9a4e076845a07
unknown <jlbarbut@ncsu.edu>
2018-04-29 16:10:04 -0400

    Full functionality and final push

--- liri.js
+++ liri.js
@@ -1,69 +1,65 @@
-
+//require npm packages and other documents
 require("dotenv").config();
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
 var keys = require('./keys.js');
 var request = require("request");
+var fs = require("fs"); 
 
+//VARIABLES ===============================================================
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
 var command = process.argv[2];
-
-//twitter api will diaplay most recent 20 tweets
-if (command === "my-tweets"){
+var arr = [];
+var arrString = ""
+for (i=3; i<process.argv.length; i++){
+    arr.push(process.argv[i]);
+}
+for (i=0; i<arr.length; i++){
+    arrString += arr[i] + " ";
+}
+ 
+// FUNCTIONS ==============================================================
+function twitterFunction(){
     // var queryURL = "https://api.twitter.com/1.1/search/tweets.json?q=nyx_the_grey&src=typd"
     var params = {screen_name: 'nyx_the_grey'};
     client.get('statuses/user_timeline', params, function(error, tweets, response){
         if (!error){
             console.log("@nyx_the_grey:")
-            for (var i=0; i<10; i++){
+            for (var i=0; i<20; i++){
                 console.log((i+1) + ". " + tweets[i].text);
             };
         }else{
             console.log("Error: " + error);
         }
     });
+}
 
-//============================================================================
-
-//user input of "spotify-this-song" will use spotify api to search for song information
-}else if (command === "spotify-this-song"){
-    console.log("hi!");
-    if (process.argv.length<=3){
-        var song = "The+Sign"; //if no user specified movie, default is "Mr. Nobody"
+function spotifyFunction(){
+    if (process.argv.length<=3 && command==="spotify-this-song"){
+        var song = "The+Sign"; //if no user specified movie, default is "The Sign"
     }else{
-        var song = process.argv[3];
-        if (process.argv.length>=4){ //builds movie title for url is title is more than one word
-            for (var i=4; i<process.argv.length; i++){
-                var song = song + "+" + process.argv[i];
-            }
-        }
+        var song = arrString;
     }
-    console.log(song);
-    // var queryUrl = "https://api.spotify.com/v1/search?q=" + song
-    spotify.request('https://api.spotify.com/v1/tracks/')
-        .then(function(data){
-            console.log(data);
-        })
-        .catch(function(err){
-            console.log("Error: " + err);
-        });
+    
+    spotify.search({type:"track", query:song}, function(error,data){
+        if(error){
+            console.log("Error: " + error);
+        }
+        console.log("Song Title: " + data.tracks.items[0].name);
+        console.log("Artist: " + (data.tracks.items[0].album.artists[0].name));
+        console.log("Link: " + data.tracks.items[0].album.external_urls.spotify);
+        console.log("Album Title: " + data.tracks.items[0].album.name);
+    
+    });
+};
 
-//===========================================================================
-
-//user input of "movie-this" will use OMBD api to search for movie information
-}else if (command === "movie-this"){
+function movieFunction(){
     if (process.argv.length<=3){
         var movie = "Mr.Nobody"; //if no user specified movie, default is "Mr. Nobody"
     }else{
-        var movie = process.argv[3];
-        if (process.argv.length>=4){ //builds movie title for url is title is more than one word
-            for (var i=4; i<process.argv.length; i++){
-                var movie = movie + "+" + process.argv[i];
-                console.log(movie);
-            }
-        }
+        var movie = arrString;
     }
     var queryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
     
@@ -77,13 +73,47 @@ if (command === "my-tweets"){
             console.log("Language: " + JSON.parse(body).Language);
             console.log("Plot: " + JSON.parse(body).Plot);
             console.log("Cast: " + JSON.parse(body).Actors);
-            // console.log(JSON.stringify(body));
+       }
+    });
+}
+
+//MAIN PROCESS ===========================================================
+
+//twitter api will diaplay most recent 20 tweets
+if (command === "my-tweets"){
+    twitterFunction();
+
+//user input of "spotify-this-song" will use spotify api to search for song information
+}else if (command === "spotify-this-song"){
+    spotifyFunction();
+
+//user input of "movie-this" will use OMBD api to search for movie information
+}else if (command === "movie-this"){
+    movieFunction();
+    
+}else if (command === "do-what-it-says"){
+    fs.readFile("random.txt", "utf8", function(error, data){
+        if (error){
+            return console.log("Error: " + error);
+        }
+        var txtArr = data.split(",");
+        for (i=0; i<txtArr.length; i++){
+        }
+        if (txtArr[0]==="spotify-this-song"){
+            arrString = txtArr[1];
+            spotifyFunction();
+
+        }else if (txtArr[0]==="my-tweets"){
+            twitterFunction();
+
+        }else if (txtArr[0]==="movie-this"){
+            arrString = txtArr[1];
+            movieFunction();
+
+        }else{
+            console.log("Invalid txt command.");
         }
     });
-
-//=============================================================================
-
-}else if (command === "do-what-it-says"){
 
 //validation that catches if the user typed something other than what is desired from the progeam
 }else{
